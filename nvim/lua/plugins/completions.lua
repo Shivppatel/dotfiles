672df66707c86f10c2907a1b2d610554e5835d29
Shivppatel <shiv.p.patel@outlook.com>
2024-05-19 00:48:27 -0400

    testing codeium plugin

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -50,6 +50,7 @@ return {
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
+					{ name = "codeium" },
 					{ name = "vim-dadbod-completion" },
 					{ name = "emoji" },
 					{ name = "path" },
