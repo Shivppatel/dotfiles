a48b84a539ce6f21ec99e7cebae59d70d2e8cd49
Shivppatel <shiv.p.patel@outlook.com>
2024-05-18 20:33:51 -0400

    added jj keymap to escape insert mode

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -13,6 +13,8 @@ vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
+vim.keymap.set("i", "jj", "<Esc>")
+
 vim.wo.number = true
 vim.o.relativenumber = true
 
