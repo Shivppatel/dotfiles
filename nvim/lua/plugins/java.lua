608441ab1ff775a67e056951d7147254a0e2087d
Shivppatel <42050563+Shivppatel@users.noreply.github.com>
2025-03-20 02:12:13 -0400

    cleanup

--- nvim/lua/plugins/java.lua
+++ nvim/lua/plugins/java.lua
@@ -4,7 +4,7 @@ return {
     ft = "java",
     config = function()
         local config = {
-            cmd = { "/Users/shivpatel/.vscode/extensions/redhat.java-1.37.0-darwin-arm64/server/bin/jdtls" },
+            cmd = { "~/.vscode/extensions/redhat.java-1.37.0-darwin-arm64/server/bin/jdtls" },
             root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
         }
         require("jdtls").start_or_attach(config)
