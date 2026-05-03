38608bcccf9000ebf81279813fe125e4e075ce55
Shivppatel <42050563+Shivppatel@users.noreply.github.com>
2026-05-02 22:23:44 -0400

    switch theme to tokyonight storm -> night

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -2,8 +2,11 @@ return {
 	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
-	opts = {},
-	config = function()
+	opts = {
+		style = "night",
+	},
+	config = function(_, opts)
+		require("tokyonight").setup(opts)
 		vim.cmd.colorscheme("tokyonight-night")
 	end,
 }
